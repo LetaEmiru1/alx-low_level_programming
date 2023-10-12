1de88216bfbf11d7959b1ba331a2f075ad3f73bd
Leta Malimo <letaemiruleta@gmail.com>
2023-10-13 00:15:01 +0300

    checkCase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,17 @@
 #include "main.h"
+
 /**
-  *main- checks for an uppercase charcter
-  *Return- 1 if affarmative 0 otherwise
-  */
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
 
 int _isupper(int c)
 {
-	if ('A' <= c && c <= 'Z')
-	{
-		return 1;
-	}
-	return 0;
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
 
-int main()
-{
-	int c;
-	int_isupper(c);
-	
-	return 0;
-}
