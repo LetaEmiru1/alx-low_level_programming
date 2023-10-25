02c9b9d4bb37b315cb284ec11dacd249bfd6cf42
Leta Malimo <letaemiruleta@gmail.com>
2023-10-25 12:01:47 +0300

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts_recursion- prints a string
+ *@s: input string
+ *Return- 0;
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\n')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
