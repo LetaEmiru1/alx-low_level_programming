022b0db13b4c254432ac242d3e47930adaeaa3f8
Leta Malimo <letaemiruleta@gmail.com>
2023-10-28 00:18:21 +0300

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- prints multiplication
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc != 3)
+	{
+		return (1);
+	}
+
+	product = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", product);
+
+	return (0);
+}
