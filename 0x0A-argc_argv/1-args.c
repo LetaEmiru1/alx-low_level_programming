73cb4cafcaeb6e7ccf35326c69ae21ccda7479e5
Leta Malimo <letaemiruleta@gmail.com>
2023-10-27 15:57:24 +0300

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main- prints the program name
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+        printf("%d\n", argc]);
+        return (0);
+}
