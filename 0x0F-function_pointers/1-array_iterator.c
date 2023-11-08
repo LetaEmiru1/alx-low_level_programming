ee431bc934d09c8ece8472c012b35f7e939db450
Leta Malimo <letaemiruleta@gmail.com>
2023-11-08 15:18:33 +0300

    minor modification

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,9 +7,9 @@
  * @action: function pointer
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, int size, void (*action)(int))
 {
-	size_t i;
+	int i;
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
