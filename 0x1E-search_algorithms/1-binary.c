f04817d712e6035e8f960ddc0e436ed731b162bd
Leta Malimo <letaemiruleta@gmail.com>
2024-05-09 01:58:30 +0300

    modified

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,51 +1,39 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
- * print_array - Helper function to print the current subarray being searched
- * @array: Pointer to the first element of the array
- * @left: Starting index of the subarray
- * @right: Ending index of the subarray
- */
-void print_array(const int *array, size_t left, size_t right)
-{
-    printf("Searching in array: ");
-    for (size_t i = left; i <= right; i++)
-    {
-        printf("%d", array[i]);
-        if (i < right)
-            printf(", ");
-    }
-    printf("\n");
-}
-
-/**
- * binary_search - Searches for a value in a sorted array of integers using the Binary search algorithm
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
- * @value: Value to search for
- * Return: The index where value is located, or -1 if not present or array is NULL
- */
+  * binary_search - Searches for a value in a sorted array
+  *                 of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+	size_t i, left, right;
 
-    if (!array)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (left <= right)
-    {
-        print_array(array, left, right);
-        mid = left + (right - left) / 2;
-        if (array[mid] == value)
-            return ((int)mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return (-1);
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
 }
-
